0e44140fe15fb757c4b6b495efc8a6eb9b8e39d8
whereismytime <a.v.dyma@gmail.com>
2025-04-05 14:58:21 +0100

    fix: Access to XMLHttpRequest... has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header...

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -4,24 +4,41 @@ using BlobStorageAPI.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// Azure Blob Storage connection
+// ?? Подключение к Azure Blob Storage
 string connectionString = builder.Configuration.GetConnectionString("AzureBlobConnection");
 builder.Services.AddSingleton(x => new BlobServiceClient(connectionString));
 builder.Services.AddScoped<IBlobRepository, BlobRepository>();
 
+// ?? Добавление базовых сервисов
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// ?? Настройка CORS для Static Web App
+var frontendUrl = "https://kind-sky-0ebf70103.6.azurestaticapps.net";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowStaticWebApp", policy =>
+    {
+        policy.WithOrigins(frontendUrl) // ?? Указываем твой фронт
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
-// Middleware
+// ?? Middleware
 app.UseSwagger();
 app.UseSwaggerUI();
 
-
 app.UseHttpsRedirection();
+
+// ?? Активируем CORS
+app.UseCors("AllowStaticWebApp");
+
 app.UseAuthorization();
 app.MapControllers();
+
 app.Run();
